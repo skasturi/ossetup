0fcfcb84c6d63cd6e3ce84a9729264a1f13d062c
Sasidhar Kasturi <skasturi@users.noreply.github.com>
2020-01-20 23:13:26 -0800

    Add bunch of packages

--- windows/initial-setup.ps1
+++ windows/initial-setup.ps1
@@ -1,3 +1,27 @@
 # By pass execution policy and install chocolatey
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y --force microsoft-edge-insider
+
+choco install -y git.install
+
+choco install -y notepadplusplus.install
+
+choco install -y vlc
+
+choco install -y 7zip.install
+
+# Insider version
+choco install -y vscode-insiders
+
+choco install -y adobereader
+
+# Adds conda to the system's PATH
+choco install -y anaconda3
+
+# From .NET Foundation
+choco install -y dotnetcore-sdk
+
+
+
